2d3b9f6506f8cb4c42c27b9c01cbf9381845f4ab
wenlilong <zndxwll@gmail.com>
2021-02-23 18:57:27 +0800

    add position

--- batch_ocr.py
+++ batch_ocr.py
@@ -1,3 +1,4 @@
+import cv2
 import codecs
 import glob
 import ocr_engine
@@ -5,6 +6,9 @@ import json
 
 scanned_folder = './data/scanned/'
 camera_folder = './data/camera/'
+# scanned_folder = './data/scanned_small/'
+# camera_folder = './data/camera_small/'
+
 
 reader = ocr_engine.Reader()
 scanned_imgs = glob.glob(scanned_folder + "*")
@@ -12,12 +16,28 @@ camera_imgs = glob.glob(camera_folder + "*")
 
 scanned_result_json = './data/scanned_result.json'
 camera_result_json = './data/camera_result.json'
+# scanned_result_json = './data/scanned_result_small.json'
+# camera_result_json = './data/camera_result_small.json'
+
 
 scanned_result_dict = {}
 for scanned_item in scanned_imgs:
-    item_result = reader.readtext(scanned_item, detail=1, paragraph=True)
+    # item_result = reader.readtext(scanned_item, detail=1, paragraph=True)
+    item_result = reader.readtext(scanned_item, detail=1, paragraph=False)
+    img = cv2.imread(scanned_item, 0)
+    h, w = img.shape
+    new_item_result = []
     print(scanned_item)
-    scanned_result_dict[str(scanned_item.split("/")[-1][:-4])] = item_result
+    for item in item_result:
+        tmp = []
+        for coord_item in item[0]:
+            new_x = round(coord_item[0] / w, 2)
+            new_y = round(coord_item[1] / h, 2)
+            tmp.append([new_x, new_y])
+
+        new_item_result.append([tmp, item[1]])
+
+    scanned_result_dict[str(scanned_item.split("/")[-1][:-4])] = str(new_item_result)
 
 with codecs.open(scanned_result_json, 'w', encoding='utf-8') as scanned_f:
     json.dump(scanned_result_dict, scanned_f, ensure_ascii=False)
@@ -25,9 +45,20 @@ with codecs.open(scanned_result_json, 'w', encoding='utf-8') as scanned_f:
 
 camera_result_dict = {}
 for camera_item in camera_imgs:
-    item_result = reader.readtext(camera_item, detail=1, paragraph=True)
+    # item_result = reader.readtext(camera_item, detail=1, paragraph=True)
+    item_result = reader.readtext(camera_item, detail=1, paragraph=False)
+    img = cv2.imread(camera_item, 0)
+    h, w = img.shape
     print(camera_item)
-    camera_result_dict[str(camera_item.split("/")[-1][:-4])] = item_result
+    new_item_result = []
+    for item in item_result:
+        tmp = []
+        for coord_item in item[0]:
+            new_x = round(coord_item[0] / w, 2)
+            new_y = round(coord_item[1] / h, 2)
+            tmp.append([new_x, new_y])
+        new_item_result.append([tmp, item[1]])
+    camera_result_dict[str(camera_item.split("/")[-1][:-4])] = str(new_item_result)
 
 with codecs.open(camera_result_json, 'w', encoding='utf-8') as camera_f:
     json.dump(camera_result_dict, camera_f, ensure_ascii=False)
